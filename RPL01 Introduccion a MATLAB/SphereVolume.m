85b3b224ff53e578c0fa84b228606d4c04bf8d1e
RationalKuma4 <ans.alv.46gmail.com>
2017-02-24 14:17:06 -0600

    Ejempo

---
+++ RPL01 Introduccion a MATLAB/SphereVolume.m
@@ -0,0 +1,6 @@
+%Ejemplo de comentario
+%radio = 5.4;
+radio = input('Radio de la esfera: ');
+volume = 4/3 * pi * radio * 3;
+disp('El volumen de la esfere: ')
+disp([num2str(radio) ' is ' num2str(volume)])

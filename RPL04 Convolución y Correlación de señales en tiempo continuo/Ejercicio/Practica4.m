3f6ee0179e7c9d71af0578fa3c14abd56c0e1df1
Anselmo Alvarado <ans.alv.46@gmail.com>
2017-05-11 22:29:12 -0500

    Funcion

---
+++ RPL04 Convolución y Correlación de señales en tiempo continuo/Ejercicio/Practica4.m
@@ -0,0 +1,26 @@
+%% Práctica 4: Convolución y Correlación de señales en tiempo continuo
+% *Alvarado Balbuena Jorge Anselmo*
+%% Problema 1
+% Este punto es clave para que se facilite tu trabajo, aunque 
+% podrás realizar el resto de los entregables aún cuando este punto no lo 
+% realices. Tienes que agregar a la función _convconm_ los párametros
+% necesarios de entrada para que las convoluciones se muestren de manera
+% correcta en su intervalo de definición, es decir, la programación actual
+% solo muestra una buena visualización de la simulación cuando las 
+% funciones están definidas en intervalos parecidos al ejemplo mostrado, 
+% tendrás que hacer cambios para que la función tenga un uso más general, 
+% esto reducira el trabajo de los sigueintes puntos a solo llamadas de esta
+% modificación, de lo contrario tendrías que entregar varias modificaciones
+% al ejemplo comentado   
+%% Problema 2
+%
+%% Problema 3
+% 
+%% Problema 4
+% 
+%% Problema 5
+% 
+%% Problema 6
+%
+%% Problema 7
+%
